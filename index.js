fca4fe0135ef668123fa3519c0061be4311258e1
Axel Somerseth Córdova López <axelsomerseth@gmail.com>
2022-03-24 14:55:12 -0600

    linting code

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import { concatArray, sumOneToAll } from './modules/arrayOperations.js';
+import { concatArray, sumOneToAll } from "./modules/arrayOperations.js";
 
 const arr1 = [1, 2, 3];
 const arr2 = [4, 5, 6];
 
-const unionArrays = concatArray(arr1, arr2); 
+const unionArrays = concatArray(arr1, arr2);
 console.debug(`Concatenated arrays: [${unionArrays}]`);
 
 const plusOne = sumOneToAll(arr1);
