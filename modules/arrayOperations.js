4c0372ac1f1f20cea62e8cd18b7a0e00fef1ce30
Axel Somerseth Córdova López <axelsomerseth@gmail.com>
2022-03-26 21:31:20 -0600

    removed unnecesary comments

--- modules/arrayOperations.js
+++ modules/arrayOperations.js
@@ -1,7 +1,5 @@
-// Pure function.
 const concatArray = (arr1, arr2) => [...arr1, ...arr2];
 
-// Pure function.
 const sumOneToAll = (arr) => arr.map((n) => n + 1);
 
 export { concatArray, sumOneToAll };
